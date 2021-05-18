2c81725c93c2cc6a9e51dbbf5abf042f6f2feb1d
radu-luca <79149783+radu-luca@users.noreply.github.com>
2021-05-18 17:57:47 +0300

    update boardpanel

--- Client/src/main/java/views/BoardPanel.java
+++ Client/src/main/java/views/BoardPanel.java
@@ -3,12 +3,18 @@ package views;
 import controllers.MyMouseListener;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BoardPanel extends JPanel {
 
+    private Dimension dimension = new Dimension(1280,720);
     private MyMouseListener listener;
     public BoardPanel(MyMouseListener listener)
     {
+        setPreferredSize(dimension);
+        setLayout(new GridLayout(8,8));
+
         this.listener = listener;
     }
+
 }
